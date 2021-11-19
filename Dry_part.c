dea7417fcbf49ae4b2cca0063a010c9a2703cd14
adigrauer <adigrauer@campus.technion.ac.il>
2021-11-19 17:25:08 +0200

    start findings the errors

--- Dry_part.c
+++ Dry_part.c
@@ -2,15 +2,16 @@
 #include <string.h>
 #include <assert.h>
 char *stringduplicator(char *s, int times) {    //convention1- function name should start with upper lettter from the second word and on 
-    assert(!s);    //coding1- we can not asume that the string isnt NULL, assert used for checking our assumptions   
+                                                //convention2- wording function names as verbs, not action names.
+    assert(!s);    //coding1- if the string s is NULL we sholud return NULL and not collapse the program  
     assert(times > 0);
     int LEN = strlen(*s);   //coding2- strlen function input is pointer. using dereferance to a pointer sends to strlen valuse instead of pointer.
-                            //convention2- variables names in lower case and not upper case.
+                            //convention3- variables names in lower case and not upper case.
     char *out = malloc(LEN * times);    //coding3- no memory allocation was made for '/0' at the end of the new srting
-    assert(out);
+    assert(out);    //coding4- if the string out is NULL we sholud return NULL and not collapse the program 
     for (int i = 0; i < times; i++) {
-    out = out + LEN;    //convention3- no indentation is used in  if block 
+    out = out + LEN;    //convention4- no indentation is used in  if block 
     strcpy(out, s);
     }
-    return out;
+    return out;   //coding5- out is not a pointer for the start of the string beacuse we promoted out pointer in the coppy loop instead of promoting coopy of the pointer. 
 }
