8c82954b0479a345011c3cc3eb10d5f61281b0d4
adigrauer <adigrauer@campus.technion.ac.il>
2021-11-19 17:55:06 +0200

    starting q2_dry

--- Dry_part.c
+++ Dry_part.c
@@ -1,21 +1,34 @@
+//q2.1
+//q2.1.1
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-char *stringduplicator(char *s, int times) {    //convention1- function name should start with upper lettter from the second word and on 
-                                                //convention2- wording function names as verbs, not action names.
-    assert(!s);    //coding1- if the string s is NULL we sholud return NULL and not collapse the program  
+char *stringduplicator(char *s, int times) {    //convention1   //convention2
+    assert(!s);    //coding1
     assert(times > 0);
-    int LEN = strlen(*s);   //coding2- strlen function input is pointer. using dereferance to a pointer sends to strlen valuse instead of pointer.
-                            //convention3- variables names in lower case and not upper case.
-    char *out = malloc(LEN * times);    //coding3- no memory allocation was made for '/0' at the end of the new srting, and need to duplicate sizeof(len) because the allocation is of bits.
-    assert(out);    //coding4- assert doent check if out is NULL but if its not. if the string out is NULL we sholud return NULL and not collapse the program. 
+    int LEN = strlen(*s);   //coding2   //convention3  
+    char *out = malloc(LEN * times);    //coding3
+    assert(out);    //coding4
     for (int i = 0; i < times; i++) {  
-    out = out + LEN;    //convention4- no indentation is used in  if block 
+    out = out + LEN;    //convention4
     strcpy(out, s);
     }
-    return out;   //coding5- out is not a pointer for the start of the string beacuse we promoted out pointer in the coppy loop instead of promoting coopy of the pointer. 
+    return out;   //coding5
 }
+//conventions errors-
+//convention1- function name should start with upper lettter from the second word and on.
+//convention2- wording function names as verbs, not action names.
+//convention3- variables names in lower case and not upper case.
+//convention4- no indentation is used in  if block. 
 
+//coding erroes-
+//coding1- if the string s is NULL we sholud return NULL and not collapse the program. 
+//coding2- strlen function input is pointer. using dereferance to a pointer sends to strlen valuse instead of pointer.
+//coding3- no memory allocation was made for '/0' at the end of the new srting, and need to duplicate sizeof(len) because the allocation is of bits.
+//coding4- assert doent check if out is NULL but if its not. if the string out is NULL we sholud return NULL and not collapse the program. 
+//coding5- out is not a pointer for the start of the string beacuse we promoted out pointer in the coppy loop instead of promoting coopy of the pointer. 
+
+//q2.1.2
 //Fixed version of the above code
 #include <stdlib.h>
 #include <string.h>
@@ -37,3 +50,22 @@ char *duplicateTheString(char *s, int times)
     }
     return out;   
 }
+
+//q2.2
+#include <stdlib.h>
+//sorted_lonkes_lists.h
+typedef struct node_t {
+    int x;
+    struct node_t *next;
+} *Node;
+
+typedef enum {
+    SUCCESS=0, MEMORY_ERROR, EMPTY_LIST, UNSORTED_LIST, NULL_ARGUMENT,
+} ErrorCode;
+
+int getListLength(Node list);
+bool isListSorted(Node list);
+ErrorCode mergeSortedLists(Node list1, Node list2, Node *mergedOut);
+
+//sorted_lonkes_lists.c
+
