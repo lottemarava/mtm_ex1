a5396dcb3678efed4780370cc9ff53ad09d2f08b
adigrauer <adigrauer@campus.technion.ac.il>
2021-11-20 02:04:34 +0200

    create h file

---
+++ sorted_linked_list.h
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+typedef struct node_t {
+    int x;
+    struct node_t *next;
+} *Node;
+
+typedef enum {
+    SUCCESS=0, 
+    MEMORY_ERROR, 
+    EMPTY_LIST, 
+    UNSORTED_LIST, 
+    NULL_ARGUMENT,
+} ErrorCode;
+
+int getListLength(Node list);
+bool isListSorted(Node list);
+ErrorCode mergeSortedLists(Node list1, Node list2, Node *mergedOut);
