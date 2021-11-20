5a452db3c03d60f72bfbf7be13522d80bfc34fe9
adigrauer <adigrauer@campus.technion.ac.il>
2021-11-20 11:30:36 +0200

    create function in part 2 dry

--- sorted_linked_list.c
+++ sorted_linked_list.c
@@ -1,7 +1,29 @@
 #include "sorted_linked_list.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 
-//Create new node with a value
+int getListLength(Node list)
+{
+    int counter=0;
+    Node list_for_count= list;
+    while(list_for_count != NULL){
+        counter++;
+        list_for_count= list_for_count->next;
+    }
+    return counter;
+}
+
+bool isListSorted(Node list){
+    if (list == NULL)
+        return true;
+    for (Node index =list ; index->next != NULL; index=index->next)
+        if (index->x > index->next->x)
+            return false;
+    return true;
+}
+
 Node createNode(int value)
 {
     Node ptr = malloc(sizeof(ptr));
@@ -14,7 +36,7 @@ Node createNode(int value)
 
 bool cheackIfValid ()
 {
-    
+
 }
 
 //Destroy the whole list
@@ -29,18 +51,26 @@ void destroyList(Node ptr)
 
 ErrorCode mergeSortedLists(Node list1, Node list2, Node *mergedOut)
 {
-    assert(isListSorted(list1) && isListSorted(list2));
-    mergedOut = malloc(sizeof(*mergedOut));
-    if(getListLength(list1) == 0)
+    Node ptr_list1= list1, ptr_list2= list2;
+    if (getListLength(ptr_list1) ==  NULL || getListLength(ptr_list2) ==  NULL)
     {
-        //  coppy list2 to  mergr
+        mergedOut=NULL;
         return EMPTY_LIST;
     }
-    if(getListLength(list2) == 0)
-    {
-        //  coppy list1 to  mergr
-        return EMPTY_LIST;
+    int smaller_value_list= compareNodes(ptr_list1, ptr_list2);
+    Node new_list = createNode(smaller_value_list);
+    if (new_list == NULL){
+        mergedOut= NULL;
+        return MEMORY_ERROR;
     }
-    
-    return SUCCESS;
+    isValid();
+
+    int value_sorted= coppyNode(smaller_value_list);
+    createNode(value_sorted);
+    if (new_list == NULL){
+        mergedOut= NULL;
+        return MEMORY_ERROR;
+    }
+
+
 }
